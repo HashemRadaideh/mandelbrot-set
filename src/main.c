cb7e4757a4e648100acab76574801ff0bc245416
HashemRadaideh <hashemalradaideh@gmail.com>
2024-11-29 04:06:04 +0300

    minor changes

--- src/main.c
+++ src/main.c
@@ -12,10 +12,10 @@
 
 uint16_t width = 0, height = 0;
 
-double realMin = -2.0, realMax = 2.0, imagMin = -1.5, imagMax = 1.5;
-double prevRealMin = 0, prevRealMax = 0, prevImagMin = 0, prevImagMax = 0;
+float realMin = -2.0, realMax = 2.0, imagMin = -1.5, imagMax = 1.5;
+float prevRealMin = 0, prevRealMax = 0, prevImagMin = 0, prevImagMax = 0;
 
-double zoomLevel = 1;
+float zoomLevel = 1;
 uint64_t lod = LOD;
 
 Color* pixels = NULL;
@@ -25,10 +25,10 @@ void initialize(uint16_t* width, uint16_t* height) {
   *width = GetScreenWidth();
   *height = GetScreenHeight();
 
-  const double aspectRatio = (double)*height / (double)*width;
-  const double realRange = realMax - realMin;
-  const double imagCenter = (imagMin + imagMax) / 2.0;
-  const double imagRange = realRange * aspectRatio;
+  const float aspectRatio = (float)*height / (float)*width;
+  const float realRange = realMax - realMin;
+  const float imagCenter = (imagMin + imagMax) / 2.0;
+  const float imagRange = realRange * aspectRatio;
 
   imagMin = imagCenter - imagRange / 2.0;
   imagMax = imagCenter + imagRange / 2.0;
@@ -44,14 +44,14 @@ void handleZoom() {
   float scroll = GetMouseWheelMove();
   if (scroll == 0) return;
 
-  const double zoomFactor = (scroll > 0) ? ZOOM_IN_FACTOR : ZOOM_OUT_FACTOR;
+  const float zoomFactor = (scroll > 0) ? ZOOM_IN_FACTOR : ZOOM_OUT_FACTOR;
   zoomLevel = zoomLevel / zoomFactor > 0 ? zoomLevel / zoomFactor : zoomLevel;
 
-  const double realCenter = (realMin + realMax) / 2.0;
-  const double imagCenter = (imagMin + imagMax) / 2.0;
+  const float realCenter = (realMin + realMax) / 2.0;
+  const float imagCenter = (imagMin + imagMax) / 2.0;
 
-  const double realRange = (realMax - realMin) * zoomFactor / 2.0;
-  const double imagRange = (imagMax - imagMin) * zoomFactor / 2.0;
+  const float realRange = (realMax - realMin) * zoomFactor / 2.0;
+  const float imagRange = (imagMax - imagMin) * zoomFactor / 2.0;
 
   realMin = realCenter - realRange;
   realMax = realCenter + realRange;
@@ -75,24 +75,27 @@ void handleDrag() {
 
   if (!isDragging) return;
 
-  const Vector2 dragDelta = Vector2Subtract(dragStart, GetMousePosition());
+  const Vector2 dragDelta = Vector2Subtract(GetMousePosition(), dragStart);
   dragStart = GetMousePosition();
 
-  const double realRange = realMax - realMin;
-  const double imagRange = imagMax - imagMin;
+  const float realRange = realMax - realMin;
+  const float imagRange = imagMax - imagMin;
 
-  realMin += (dragDelta.x / width) * realRange;
-  realMax += (dragDelta.x / width) * realRange;
-  imagMin -= (dragDelta.y / height) * imagRange;
-  imagMax -= (dragDelta.y / height) * imagRange;
+  const float realOffset = (dragDelta.x / width) * realRange;
+  const float imagOffset = (dragDelta.y / height) * imagRange;
+
+  realMin -= realOffset;
+  realMax -= realOffset;
+  imagMin += imagOffset;
+  imagMax += imagOffset;
 }
 
-inline uint64_t mandelbrot(double real, double imag) {
-  double zReal = real;
-  double zImag = imag;
+inline uint64_t mandelbrot(float real, float imag) {
+  float zReal = real;
+  float zImag = imag;
 
-  double zReal2 = zReal * zReal;
-  double zImag2 = zImag * zImag;
+  float zReal2 = zReal * zReal;
+  float zImag2 = zImag * zImag;
 
   uint64_t iter = 0;
   for (iter = 0; zReal2 + zImag2 <= lod && iter < lod; iter++) {
@@ -107,30 +110,29 @@ inline uint64_t mandelbrot(double real, double imag) {
 }
 
 Color mapColor(uint64_t iter) {
-  return (iter == lod) ? BLACK
-                       : (Color){(iter * 5) & 255, (iter * 3) & 255,
-                                 255 - (iter & 255), 255};
+  return (Color){(iter * 5) & 255, (iter * 3) & 255, (255 - iter) & 255, 255};
 }
 
 void mandelbrotSet() {
-  const double imagStep = (imagMax - imagMin) / (height - 1);
-  double imagValues[height];
+  const float imagStep = (imagMax - imagMin) / (height - 1);
+  float imagValues[height];
   for (uint16_t y = 0; y < height; ++y) imagValues[y] = imagMax - y * imagStep;
 
-  const double realStep = (realMax - realMin) / (width - 1);
-  double realValues[width];
+  const float realStep = (realMax - realMin) / (width - 1);
+  float realValues[width];
   for (uint16_t x = 0; x < width; ++x) realValues[x] = realMin + x * realStep;
 
   for (uint16_t y = 0; y < height; ++y) {
-    const double imag = imagValues[y];
+    const float imag = imagValues[y];
 
     for (uint16_t x = 0; x < width; ++x) {
-      const double real = realValues[x];
+      const float real = realValues[x];
 
       uint64_t index = y * width + x;
       uint64_t iter = mandelbrot(real, imag);
+      Color color = iter == lod ? BLACK : mapColor(iter);
 
-      pixels[index] = mapColor(iter);
+      pixels[index] = color;
     }
   }
 }
