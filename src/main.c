ae9bd555453a57e2d74cedc30a534e6291fedad9
HashemRadaideh <hashemalradaideh@gmail.com>
2024-11-30 00:23:57 +0300

    Precomputing color palatte

--- src/main.c
+++ src/main.c
@@ -12,17 +12,20 @@
 
 uint16_t width = 0, height = 0;
 
-double realMin = -2.0, realMax = 2.0, imagMin = -1.5, imagMax = 1.5;
-double prevRealMin = 0, prevRealMax = 0, prevImagMin = 0, prevImagMax = 0;
+Vector2 dragStart = {0, 0};
+bool isDragging = false;
 
 double zoomLevel = 1;
 uint64_t lod = LOD;
 
+double realMin = -2.0, realMax = 2.0, imagMin = -1.5, imagMax = 1.5;
+double prevRealMin = 0, prevRealMax = 0, prevImagMin = 0, prevImagMax = 0;
+
+uint64_t prevLod = 0;
+Color* colorPalette = NULL;
+
 Color* pixels = NULL;
 Texture2D texture;
-
-Vector2 dragStart = {0, 0};
-bool isDragging = false;
 
 void initialize(uint16_t* width, uint16_t* height) {
   *width = GetScreenWidth();
@@ -36,6 +39,9 @@ void initialize(uint16_t* width, uint16_t* height) {
   imagMin = imagCenter - imagRange / 2.0;
   imagMax = imagCenter + imagRange / 2.0;
 
+  if (colorPalette) free(colorPalette);
+  colorPalette = malloc(MAX_LOD * sizeof(Color));
+
   if (pixels) free(pixels);
   pixels = malloc((*width) * (*height) * sizeof(Color));
 
@@ -43,23 +49,53 @@ void initialize(uint16_t* width, uint16_t* height) {
   texture = LoadTextureFromImage(GenImageColor(*width, *height, BLANK));
 }
 
-uint64_t mandelbrot(const double real, const double imag) {
-  double zReal = real;
-  double zImag = imag;
+void handleZoom() {
+  const double scroll = GetMouseWheelMove();
+  if (!scroll) return;
 
-  double zReal2 = zReal * zReal;
-  double zImag2 = zImag * zImag;
+  const double zoomFactor = scroll > 0 ? ZOOM_IN_FACTOR : ZOOM_OUT_FACTOR;
+  zoomLevel = zoomLevel / zoomFactor > 0 ? zoomLevel / zoomFactor : zoomLevel;
 
-  uint64_t iter = 0;
-  for (iter = 0; zReal2 + zImag2 <= lod && iter < lod; iter++) {
-    zImag = 2 * zReal * zImag + imag;
-    zReal = zReal2 - zImag2 + real;
+  const double realCenter = (realMin + realMax) / 2.0;
+  const double imagCenter = (imagMin + imagMax) / 2.0;
 
-    zReal2 = zReal * zReal;
-    zImag2 = zImag * zImag;
+  const double realRange = (realMax - realMin) * zoomFactor / 2.0;
+  const double imagRange = (imagMax - imagMin) * zoomFactor / 2.0;
+
+  realMin = realCenter - realRange;
+  realMax = realCenter + realRange;
+  imagMin = imagCenter - imagRange;
+  imagMax = imagCenter + imagRange;
+
+  lod = fmax(MIN_LOD, fmin(MAX_LOD, LOD + (log10(zoomLevel) * 20)));
+}
+
+void handleDrag() {
+  if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+    dragStart = GetMousePosition();
+    isDragging = true;
+  } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+    isDragging = false;
   }
 
-  return iter;
+  if (!isDragging) return;
+
+  const Vector2 dragDelta = Vector2Subtract(GetMousePosition(), dragStart);
+  dragStart = GetMousePosition();
+
+  const double realRange = realMax - realMin;
+  const double imagRange = imagMax - imagMin;
+
+  const double realOffset = (dragDelta.x / width) * realRange;
+  const double imagOffset = (dragDelta.y / height) * imagRange;
+
+  const double realCenter = (realMin + realMax) / 2.0 - realOffset;
+  const double imagCenter = (imagMin + imagMax) / 2.0 + imagOffset;
+
+  realMin = realCenter - realRange / 2.0;
+  realMax = realCenter + realRange / 2.0;
+  imagMin = imagCenter - imagRange / 2.0;
+  imagMax = imagCenter + imagRange / 2.0;
 }
 
 Color mapColor(const int iter) {
@@ -105,6 +141,32 @@ Color mapColor(const int iter) {
   return (Color){r * 255, g * 255, b * 255, 255};
 }
 
+void precomputeColor() {
+  if (prevLod == lod) return;
+
+  if (prevLod > lod) prevLod = 0;
+
+  for (; prevLod <= lod; prevLod++) {
+    colorPalette[prevLod] = mapColor(prevLod);
+  }
+}
+
+uint64_t mandelbrot(const double real, const double imag) {
+  double zReal = real, zImag = imag;
+  double zReal2 = zReal * zReal, zImag2 = zImag * zImag;
+
+  uint64_t iter = 0;
+  for (iter = 0; zReal2 + zImag2 <= lod && iter < lod; iter++) {
+    zImag = 2 * zReal * zImag + imag;
+    zReal = zReal2 - zImag2 + real;
+
+    zReal2 = zReal * zReal;
+    zImag2 = zImag * zImag;
+  }
+
+  return iter;
+}
+
 void mandelbrotSet() {
   const double imagStep = (imagMax - imagMin) / (height - 1);
   const double realStep = (realMax - realMin) / (width - 1);
@@ -115,14 +177,12 @@ void mandelbrotSet() {
     for (uint16_t x = 0; x < width; ++x) {
       const double real = realMin + x * realStep;
 
-      uint64_t iter = mandelbrot(real, imag);
-
-      pixels[y * width + x] = mapColor(iter);
+      pixels[y * width + x] = colorPalette[mandelbrot(real, imag)];
     }
   }
 }
 
-void updateSetTexture() {
+void generateTexture() {
   if (prevRealMin == realMin && prevRealMax == realMax &&
       prevImagMin == imagMin && prevImagMax == imagMax) {
     return;
@@ -137,60 +197,10 @@ void updateSetTexture() {
   UpdateTexture(texture, pixels);
 }
 
-void handleZoom() {
-  const double scroll = GetMouseWheelMove();
-  if (!scroll) return;
-
-  const double zoomFactor = scroll > 0 ? ZOOM_IN_FACTOR : ZOOM_OUT_FACTOR;
-  zoomLevel = zoomLevel / zoomFactor > 0 ? zoomLevel / zoomFactor : zoomLevel;
-
-  const double realCenter = (realMin + realMax) / 2.0;
-  const double imagCenter = (imagMin + imagMax) / 2.0;
-
-  const double realRange = (realMax - realMin) * zoomFactor / 2.0;
-  const double imagRange = (imagMax - imagMin) * zoomFactor / 2.0;
-
-  realMin = realCenter - realRange;
-  realMax = realCenter + realRange;
-  imagMin = imagCenter - imagRange;
-  imagMax = imagCenter + imagRange;
-
-  lod = LOD + (log10(zoomLevel) * 20);
-  lod = fmax(MIN_LOD, lod);
-}
-
-void handleDrag() {
-  if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-    dragStart = GetMousePosition();
-    isDragging = true;
-  } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-    isDragging = false;
-  }
-
-  if (!isDragging) return;
-
-  const Vector2 dragDelta = Vector2Subtract(GetMousePosition(), dragStart);
-  dragStart = GetMousePosition();
-
-  const double realRange = realMax - realMin;
-  const double imagRange = imagMax - imagMin;
-
-  const double realOffset = (dragDelta.x / width) * realRange;
-  const double imagOffset = (dragDelta.y / height) * imagRange;
-
-  const double realCenter = (realMin + realMax) / 2.0 - realOffset;
-  const double imagCenter = (imagMin + imagMax) / 2.0 + imagOffset;
-
-  realMin = realCenter - realRange / 2.0;
-  realMax = realCenter + realRange / 2.0;
-  imagMin = imagCenter - imagRange / 2.0;
-  imagMax = imagCenter + imagRange / 2.0;
-}
-
 int main() {
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(width, height, "Mandelbrot Set");
-  SetTargetFPS(60);
+  SetTargetFPS(0);
   initialize(&width, &height);
 
   while (!WindowShouldClose()) {
@@ -198,18 +208,21 @@ int main() {
       initialize(&width, &height);
     }
 
-    BeginDrawing();
-    ClearBackground(BLACK);
-    updateSetTexture();
-    DrawTexture(texture, 0, 0, WHITE);
-    EndDrawing();
-
     handleZoom();
     handleDrag();
+
+    precomputeColor();
+    generateTexture();
+
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawTexture(texture, 0, 0, WHITE);
+    EndDrawing();
   }
 
   UnloadTexture(texture);
   free(pixels);
+  free(colorPalette);
   CloseWindow();
   return EXIT_SUCCESS;
 }
